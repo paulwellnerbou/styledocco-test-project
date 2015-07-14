b1ec6a9d9699315c5827c7d9edbd37e8bf07937c
Paul Wellner Bou <paul@wellnerbou.de>
2015-07-14 09:35:04 +0200

    Add styledocco task to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,17 @@
-module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-serve');
+module.exports = function (grunt) {
+	grunt.initConfig({
+		styledocco: {
+			dist: {
+				options: {
+					name: 'Styledocco Test Project'
+				},
+				files: {
+					'generated/css': 'css'
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-serve');
+	grunt.loadNpmTasks('grunt-styledocco');
 };
